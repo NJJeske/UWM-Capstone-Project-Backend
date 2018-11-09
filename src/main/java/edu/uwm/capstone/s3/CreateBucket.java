79860f432453001e39048164de7577d3b9c53d73
minshinkim <minshin@uwm.edu>
2018-11-09 13:13:35 -0600

    Added dependencies for amazon aws s3 to the maven pom.xml

---
+++ src/main/java/edu/uwm/capstone/s3/CreateBucket.java
@@ -0,0 +1,11 @@
+package edu.uwm.capstone.s3;
+
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.Bucket;
+import java.util.List;
+
+public class CreateBucket {
+
+}
