336fc961cb6cfa6c87c3a909ab1ed99ecd069e0a
minshinkim <minshin@uwm.edu>
2018-12-09 11:45:57 -0600

    Added a fix for s3 client instantiation in S3Config by adding region.

--- src/main/java/edu/uwm/capstone/s3/S3Config.java
+++ src/main/java/edu/uwm/capstone/s3/S3Config.java
@@ -3,6 +3,7 @@ package edu.uwm.capstone.s3;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,12 +11,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-    // TODO - Do we need to define a region?
-
     @Bean
     public AmazonS3 s3client(){
         BasicAWSCredentials credentials = new BasicAWSCredentials("AKIAIAR6QLI6XLWJ4FWQ", "lHELWR4eRGlS0J9kPNS3S8AFYRouA+Jc1Mo2R+Qk");
         AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.fromName("us-east-2"))
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .build();
 
